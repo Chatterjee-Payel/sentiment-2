53cb2bc86ec3ed5d3c9fae8b111347b27d26d7f2
Chatterjee-Payel <98272915+Chatterjee-Payel@users.noreply.github.com>
2023-06-12 09:01:33 +0530

    Create main.py

---
+++ main.py
@@ -0,0 +1,16 @@
+from textblob import TextBlob
+import pandas as pd
+import streamlit as st
+import cleantext
+
+
+st.header('Sentiment Analysis')
+with st.expander('Analyze Text'):
+      text = st.text_input("Text here: ")
+      if text:
+        blob = TextBlob(text)
+        st.write('Polarity: ', round(blob.sentiment.polarity,2))
+        st.write('Subjectivity: ', round(blob.sentiment.subjectivity,2))
+
+
+
